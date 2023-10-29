9aa39c927ac23b0ffd75d434c84686c667ceb1a4
alima987 <alima1996b@gmail.com>
2023-10-29 21:17:09 +0500

    feat: split into components

---
+++ src/components/Search.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent } from 'react';
+
+interface SearchProps {
+  handleSearch: () => void;
+  searchTerm: string;
+  handleSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search: React.FC<SearchProps> = ({
+  handleSearch,
+  searchTerm,
+  handleSearchChange,
+}) => {
+  const render = () => {
+    return (
+      <div className="search">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+        />
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    );
+  };
+
+  return render();
+};
+
+export default Search;
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface Starship {
+  name: string;
+  model: string;
+  manufacturer: string;
+}
+
+interface SearchResultsProps {
+  searchResults: Starship[];
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({ searchResults }) => {
+  const render = () => {
+    return (
+      <div className="bottom-section">
+        {searchResults.map((result) => (
+          <div key={result.name}>
+            <h2>{result.name}</h2>
+            <p>{result.model}</p>
+            <p>{result.manufacturer}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  return render();
+};
+
+export default SearchResults;
