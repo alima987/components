4c7b6e95f9f22f47becafc8059caaf61f1b1968a
alima987 <alima1996b@gmail.com>
2023-10-31 21:06:34 +0500

    refactor: fix configs

--- src/App.tsx
+++ src/App.tsx
@@ -9,43 +9,13 @@ interface Starship {
   manufacturer: string;
 }
 
-interface ErrorBoundaryProps {
-  children: React.ReactNode;
-}
-
-interface ErrorBoundaryState {
-  hasError: boolean;
-  errorMessage: string;
-}
-
-class ErrorBoundary extends React.Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false, errorMessage: '' };
-  }
-
-  componentDidCatch(error: Error) {
-    this.setState({ hasError: true, errorMessage: error.message });
-  }
-
-  render() {
-    const { children } = this.props;
-    const { hasError, errorMessage } = this.state;
-    if (hasError) {
-      return <p>{errorMessage}</p>;
-    }
-    return children;
-  }
-}
-
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>(
     localStorage.getItem('searchTerm') || ''
   );
   const [searchResults, setSearchResults] = useState<Starship[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchData = useCallback(async () => {
     try {
@@ -56,15 +26,12 @@ function App() {
       const response = await fetch(url);
       const data = await response.json();
       setSearchResults(data.results);
+      localStorage.setItem('searchResults', JSON.stringify(data.results));
     } catch (error) {
-      throw new Error('Something went wrong. Please try again later.');
+      setHasError(true);
+      setErrorMessage('Something went wrong. Please try again later.');
     }
   }, [searchTerm]);
-
-  useEffect(() => {
-    fetchData();
-    localStorage.setItem('searchTerm', searchTerm);
-  }, [fetchData, searchTerm]);
 
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     const term = event.target.value.trim();
@@ -79,20 +46,29 @@ function App() {
     throw new Error('Test error thrown by the button click');
   };
 
+  useEffect(() => {
+    fetchData();
+    localStorage.setItem('searchTerm', searchTerm);
+  }, [fetchData, searchTerm]);
+
   return (
-    <ErrorBoundary>
-      <div className="App">
-        <Search
-          handleSearch={handleSearch}
-          searchTerm={searchTerm}
-          handleSearchChange={handleSearchChange}
-        />
-        <SearchResults searchResults={searchResults} />
-        <button type="button" onClick={throwError}>
-          Error
-        </button>
-      </div>
-    </ErrorBoundary>
+    <div className="App">
+      {hasError ? (
+        <p>{errorMessage}</p>
+      ) : (
+        <>
+          <Search
+            handleSearch={handleSearch}
+            searchTerm={searchTerm}
+            handleSearchChange={handleSearchChange}
+          />
+          <SearchResults searchResults={searchResults} />
+          <button type="button" onClick={throwError}>
+            Error
+          </button>
+        </>
+      )}
+    </div>
   );
 }
 
